9b96e57181790da06aa8b245e9c2aba9129dba31
Dzung Nguyen <dungnt@cogini.com>
2015-12-14 17:49:44 +0700

    - add .gitignore file to ignore atom tags dir - parse slide before parse markdown text

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-$(window).keydown(function(event) {
-    if (event.ctrlKey && event.keyCode == 82) {
-        var markdownText = $('#markdown-text').val();
-        $('#html-text').html(marked(markdownText));
-        event.preventDefault();
+var slideReg = /^ *::([a-zA-Z0-9]*).*\n*([^]*?)(?:\n+ *::)/;
+function compile(text){
+    var html = "";
+    while((result = slideReg.exec(text)) != undefined){
+      html += '<div>';
+      html += marked(result[2]);
+      html += "</div>";
+      text = text.substring(result[0].length-2);
     }
-    if (event.ctrlKey && event.keyCode == 83) {
-        console.log("Hey! Ctrl+S event captured!");
-        event.preventDefault();
-    }
+    return html;
+}
+
+$(document).ready(function(e){
+  $(window).keydown(function(event) {
+      if (event.ctrlKey && event.keyCode == 82) {
+          var markdownText = $('#markdown-text').val();
+          $('#html-text').html(compile(markdownText));
+          event.preventDefault();
+          event.stopPropagation();
+      }
+      if (event.ctrlKey && event.keyCode == 83) {
+          console.log("Hey! Ctrl+S event captured!");
+          event.preventDefault();
+      }
+  });
+
+  $('#compile').click(function(e){
+    var markdownText = $('#markdown-text').val();
+    $('#html-text').html(compile(markdownText));
+    event.preventDefault();
+  });
 });
